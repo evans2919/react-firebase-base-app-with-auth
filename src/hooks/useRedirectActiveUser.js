854792632d6dff86b984aafb82671125c27a9972
evans2919 <evansalcantara29@outlook.com>
2023-09-26 19:17:47 +0200

    Delete context

--- src/hooks/useRedirectActiveUser.js
+++ src/hooks/useRedirectActiveUser.js
@@ -1,15 +1,13 @@
 import { useEffect } from "react";
-// import { useUserContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 
 const useRedirectActiveUser = (user, path) => {
-//   const { user } = useUserContext();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (user) {
-      navigate(path);
-    }
-  }, [user]);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (user) {
+            navigate(path);
+        }
+    }, [user]);
 };
 
 export default useRedirectActiveUser;
