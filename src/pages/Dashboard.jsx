d0214a6ce8266547f598d23b35038be4c4ed8de1
evans2919 <evansalcantara29@outlook.com>
2023-10-04 17:40:27 +0200

    Add mui button

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,24 +1,27 @@
+import { Button } from "@mui/material";
 import { logout } from "../config/firebase";
 import { useUserContext } from "../context/UserContext";
 import usePageTitle from "../hooks/usePageTitle";
 
 const Dashboard = () => {
-  usePageTitle();
-  const { user } = useUserContext();
-  const handleLogout = async () => {
-    try {
-      logout();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <>
-      <h1>Dashboard</h1>
-      <p>Email: {user.email}</p>
-      <button onClick={handleLogout}>Logout</button>
-    </>
-  );
+    usePageTitle();
+    const { user } = useUserContext();
+    const handleLogout = async () => {
+        try {
+            logout();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <>
+            <h1>Dashboard</h1>
+            <p>Email: {user.email}</p>
+            <Button variant="contained" onClick={handleLogout}>
+                Logout
+            </Button>
+        </>
+    );
 };
 
 export default Dashboard;
